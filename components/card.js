90af625f94d879b6de30d445e5caeb99c9aff83a
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-03-26 23:33:13 +0530

    side shoe card

--- components/card.js
+++ components/card.js
@@ -1,16 +1,18 @@
 export const Card = () => {
     return (
       <>
-        <div className="card w-44 glass">
+        <div class="card w-35 bg-base-80 shadow-xl m-2 card-side	">
           <figure>
             <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="car!"
+              src="https://media.istockphoto.com/photos/pair-of-stylish-sneakers-isolated-on-white-background-white-casual-picture-id1248566148?k=20&m=1248566148&s=170667a&w=0&h=Lom1xUV8pfz2kFmzepcEzn52mlEzWj5d2TMDFKlhOsc="
+              alt="Shoes"
+              width="60"
+              height="60"
             />
           </figure>
-          <div className="card-body">
-            <p>Half dome palover</p>
-            <h2 className="card-title">$87</h2>
+          <div class="card-body">
+            <h2 class="card-title">$87</h2>
+            <p>Air Jordans</p>
           </div>
         </div>
       </>
