0790459727aef0fb59c0f6498c00d97022c6d1fe
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-03-27 09:39:02 +0530

    card update

--- components/card.js
+++ components/card.js
@@ -2,14 +2,14 @@ export const Card = () => {
     return (
       <>
         <div
-          class=" w-60 h-40 rounded-2xl	card 	mr-2 shadow-xl"
+          className=" w-60 h-40 rounded-2xl	card 	mr-2 shadow-xl"
           style={{ backgroundSize: "10 10px" }}
         >
-          <div class="m-3 p-5">
+          <div className="m-3 p-5">
             <p className="mt-2">
               Half dome palover <br /> shoes
             </p>
-            <h2 class="card-title">$87</h2>
+            <h2 className="card-title">$87</h2>
           </div>
         </div>
       </>
